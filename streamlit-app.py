740b5387c7416bb182b28cf8618d1f1ca5d751fd
monaj22 <jawadmona22@gmail.com>
2025-06-24 13:17:08 -0500

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -13,19 +13,7 @@ def find_nearest_idx(array, value):
     idx = (np.abs(array - value)).argmin()
     return idx
 
-def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
-    htmlstr = f"""
-        <script>
-            var elements = window.parent.document.querySelectorAll('button');
-            for (var i = 0; i < elements.length; ++i) {{
-                if (elements[i].innerText == '{widget_label}') {{
-                    elements[i].style.color ='{font_color}';
-                    elements[i].style.background = '{background_color}'
-                }}
-            }}
-        </script>
-        """
-    components.html(f"{htmlstr}", height=0, width=0)
+
 
 
 def noise_floor_calculation(first_harmonic_freq, freq_array, amplitude_array):
@@ -62,7 +50,6 @@ def noise_floor_calculation(first_harmonic_freq, freq_array, amplitude_array):
 
 def sum_harmonics_by_peak(fft_data,freq_vector, limit):
     # Take the magnitude of the FFT data
-    #TODO: Create limit for where peaks are found based on file
     #Find cut-off index of limit (which is the frequency we are testing at, e.g 250 or 500 or 1000)
     #Index isn't exact due to how freq_vector is formed
     cutoff_index = np.argmin(np.abs(freq_vector - limit))
@@ -106,12 +93,10 @@ def extract_data(df, trace_type):
     
 
 def parse_xml(uploaded_file):
-        # Parse the XML file
     Tree = ETree.parse(uploaded_file)
     root = Tree.getroot()
 
-    # SHEET 1 - ECOCHG Summary
-    A = []  # Assign empty list to use later
+    A = []  
     for elem in root.iter('Measurements'):
         for subelem in elem:
             B = {}
@@ -439,124 +424,4 @@ if len(uploaded_file_list) > 0:
     plt.tight_layout()
     st.pyplot(fig)
 
-####Code Graveyard
-        # if (type == "Round Window"):
-        #     st.write("Round window chosen")
-        #     print("round window")
-        #     condensation_data,rarefaction_data,sum_data,difference_data,ECochG_series = parse_xml(uploaded_file_list[0])
-        #     list_xp = [1,2,3,4]
-        #     list_hz = ["200","500","1000","2000"]
-        #
-        #     # Create a single figure with 4 subplots
-        #     fig, axs = plt.subplots(2,2, figsize=(10, 20))
-        #     axs = axs.flatten()
-        #
-        #     dif_fig, dif_axs = plt.subplots(2,2,figsize=(10,20))
-        #     dif_axs = dif_axs.flatten()
-        #     # fig.tight_layout(pad=4.0)
-        #     harmonic_data = []
-        #     for index, x_p in enumerate(list_xp):
-        #         hz = list_hz[index]
-        #         df = difference_data.loc[difference_data['Measurement Number'] == x_p]
-        #         recording_electrode = ECochG_Series.loc[ECochG_Series['Measurement Number'] == x_p, 'RecordingActiveElectrode'].values
-        #
-        #         df = df.astype("float")
-        #         df = df.dropna()
-        #
-        #         # Extract time and voltage
-        #         time = df['Time(us)'] * 100
-        #         voltage = df['Sample(uV)']
-        #         # Fast Fourier Transformation
-        #         # st.subheader(f"Fast Fourier Transformation for Measurement {x_p} ({hz} Hz)")
-        #         differencefft250 = voltage
-        #
-        #
-        #         dif_axs[index].set_title(f"R-C Difference \n  {hz} Hz")
-        #         dif_axs[index].set_xlabel('Time (us)')
-        #         dif_axs[index].set_ylabel("Sample (uV)")
-        #         dif_axs[index].plot(time, voltage)
-        #
-        #         # Sampling frequency
-        #         Fs250 = 20900  # Hz
-        #
-        #         # Signal length
-        #         L250 = len(differencefft250)
-        #
-        #         # Zero-padding length
-        #         NFFT250 = 2 ** (int(np.ceil(np.log2(L250))) + 2)
-        #
-        #         # Compute the FFT and normalize
-        #         Y250 = np.fft.fft(differencefft250, NFFT250) / L250
-        #
-        #         # Frequency vector
-        #         freq_array = Fs250 / 2 * np.linspace(0, 1, NFFT250 // 2 + 1)
-        #         # Amplitude
-        #         amplitude = 2 * np.abs(Y250[:NFFT250 // 2 + 1])
-        #
-        #         # Find harmonics
-        #         harmonic_sum, fundamental_index, threshold = sum_harmonics_by_peak(amplitude)
-        #
-        #         # Get the frequencies and amplitudes corresponding to the harmonics
-        #         fundamental_freq = freq_array[fundamental_index]  # First harmonic frequency
-        #         fundamental_amp = amplitude[fundamental_index]  # Amplitude at the first harmonic
-        #
-        #         second_harmonic_freq = freq_array[2 * fundamental_index] if 2 * fundamental_index < len(freq_array) else None
-        #         second_harmonic_amp = amplitude[2 * fundamental_index] if 2 * fundamental_index < len(amplitude) else 0
-        #
-        #         third_harmonic_freq = freq_array[3 * fundamental_index] if 3 * fundamental_index < len(freq_array) else None
-        #         third_harmonic_amp = amplitude[3 * fundamental_index] if 3 * fundamental_index < len(amplitude) else 0
-        #
-        #         # Sum of the harmonics
-        #         total_amp = fundamental_amp + second_harmonic_amp + third_harmonic_amp
-        #
-        #         # Append the data for this measurement to the list
-        #         harmonic_data.append({
-        #             'Measurement Number': x_p,
-        #             'Fundamental Frequency (Hz)': fundamental_freq,
-        #             'Fundamental Amplitude': fundamental_amp,
-        #             'Second Harmonic Frequency (Hz)': second_harmonic_freq,
-        #             'Second Harmonic Amplitude': second_harmonic_amp,
-        #             'Third Harmonic Frequency (Hz)': third_harmonic_freq,
-        #             'Third Harmonic Amplitude': third_harmonic_amp,
-        #             'Total Amplitude': total_amp
-        #         })
-        #
-        #         # Plot the FFT data with harmonic lines in the respective subplot
-        #         axs[index].plot(freq_array, amplitude, linewidth=1.0)
-        #         axs[index].set_xlim([0, 9000])
-        #         axs[index].set_title(f'FFT {hz} Hz of Difference')
-        #         axs[index].set_xlabel('Frequency (Hz)')
-        #         axs[index].set_ylabel('Amplitude (microvolts)')
-        #
-        #         axs[index].plot(fundamental_freq, amplitude[fundamental_index], 'r*', markersize=10,
-        #                         label=f'1st Harmonic: {int(fundamental_freq)} Hz')
-        #         if second_harmonic_freq:
-        #             axs[index].plot(second_harmonic_freq, amplitude[2 * fundamental_index], 'g*', markersize=10,
-        #                             label=f'2nd Harmonic: {second_harmonic_freq:.2f} Hz')
-        #         if third_harmonic_freq:
-        #             axs[index].plot(third_harmonic_freq, amplitude[3 * fundamental_index], 'b*', markersize=10,
-        #                             label=f'3rd Harmonic: {third_harmonic_freq:.2f} Hz')
-        #
-        #         # Show legend for each subplot
-        #         axs[index].legend()
-        #
-        #     # After the loop, create the DataFrame from the list of harmonic data
-        #
-        #     # Display the DataFrame in Streamlit
-        #
-        #     st.pyplot(dif_fig)
-        #     st.subheader("Fourier Transform and Total Response")
-        #     harmonic_df = pd.DataFrame(harmonic_data)
-        #     total_response = sum(harmonic_df["Total Amplitude"])
-        #     db_response = control.mag2db(total_response)
-        #     # st.write(f"Total Response:  \n  {total_response} microvolts   \n   {db_response} dB")
-        #     st.write(harmonic_df)
-        #     st.pyplot(fig)
 
-
-#######SUMMARY OF NEXT STEPS
-
-#1: The second harmonic should be extracted from the SUM not the DIFFERENCE (DONE)
-#2: We need to window the SUM/DIFFERENCE to only be taking data from 6-15s into account (DONE)
-#3: The BFTR should be calculated from the electrode with the highest fundamental frequency (DONE)
-#4: We want a table with the individual harmonics and the BFTRs highlighted.
